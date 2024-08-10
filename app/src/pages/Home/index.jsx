4450c48a2c288f5b9df0b3a7c770dfeb7e94a413
Laura Ricci <lauraricci.lrc@gmail.com>
2024-08-10 14:27:11 -0300

    Add container component

--- app/src/pages/Home/index.jsx
+++ app/src/pages/Home/index.jsx
@@ -1,26 +1,30 @@
 import { Link } from "react-router-dom";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
+import Container from "../../components/Container";
+import styles from './Home.module.css';
 
 function Home() {
     return (
         <>
         <Header />
-        <section className="container">
-          <div>
-            <p>
-              Olá, sou <br/>
-              <span>Laura Ricci</span><br/>
-              Dev Full Stack
-            </p>
-            <Link to="/sobre" className="btn btn-red">
-              Saiba mais sobre mim
-            </Link>
-          </div>
-          <figure>
-            <img className="img-home" src="/developer-red.svg" alt="Imagem de Home"/>
-          </figure>
-        </section>
+        <Container>
+          <section className={styles.home}>
+            <div>
+              <p>
+                Olá, sou <br/>
+                <span>Laura Ricci</span><br/>
+                Dev Full Stack
+              </p>
+              <Link to="/sobre" className="btn btn-red">
+                Saiba mais sobre mim
+              </Link>
+            </div>
+            <figure>
+              <img className="img-home" src="/developer-red.svg" alt="Imagem de Home"/>
+            </figure>
+          </section>
+        </Container>
         <Footer />
       </>
     )
