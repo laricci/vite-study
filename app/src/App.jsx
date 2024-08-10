827e72799ce158bb2d647f07fd7f49952e07e0a0
Laura Ricci <lauraricci.lrc@gmail.com>
2024-08-10 10:19:07 -0300

    Add basic layout

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,8 +1,27 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import './App.css';
 
 function App() {
   return (
     <>
-      <h1>Hello world!</h1>
+      <Header />
+      <section className="container">
+        <div>
+          <p>
+            Olá, sou <br/>
+            <span>Laura Ricci</span><br/>
+            Dev Full Stack
+          </p>
+          <button type="button" className="btn btn-red">
+            Saiba mais sobre mim
+          </button>
+        </div>
+        <figure>
+          <img className="img-home" src="/developer-red.svg" alt="Imagem de Home"/>
+        </figure>
+      </section>
+      <Footer />
     </>
   )
 }
---
+++ app/src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from './Footer.module.css'
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            Copyrights © Laura Ricci
+        </footer>
+    )
+}
+
+export default Footer;
